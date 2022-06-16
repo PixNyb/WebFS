419d314e827b78447f3c091fd585f28b74035761
Pixel-Null <rcacouwenberg@gmail.com>
2022-06-16 18:24:43 +0000

    Wajow

---
+++ resources/views/components/admin/page.blade.php
@@ -0,0 +1,22 @@
+<!-- Standard HTML5 page -->
+@props([
+"page" => '',
+"category" => config('app.name')
+])
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/normalise.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+    <title>{{ ($page == '' ? '' : $page . ' - ') . $category }}</title>
+</head>
+
+<body>
+</body>
+
+</html>
---
+++ resources/views/components/app/page.blade.php
@@ -0,0 +1,64 @@
+<!-- Standard HTML5 page -->
+@props([
+"page" => '',
+"category" => config('app.name')
+])
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/normalise.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>{{ ($page == '' ? '' : $page . ' - ') . $category }}</title>
+</head>
+
+<body>
+    <header>
+        <div>
+            <span>
+                <img src="{{ asset('images/dragon-small.png') }}" alt="" />
+                <span>{{ __('header.name') }}</span>
+                <img src="{{ asset('images/dragon-small-flipped.png') }}" alt="" />
+            </span>
+        </div>
+        <div>
+            <a href="#">
+                <marquee behavior="scroll" direction="left">
+                    {{ __('header.scroller') }}
+                </marquee>
+            </a>
+        </div>
+        <div>
+            <span>
+                <img src="{{ asset('images/dragon-small.png') }}" alt="" />
+                <span>{{ __('header.name') }}</span>
+                <img src="{{ asset('images/dragon-small-flipped.png') }}" alt="" />
+            </span>
+        </div>
+    </header>
+    <main>
+        <aside class="flex row">
+            <img src="{{ asset('images/dragon-small.png') }}" alt="" />
+            <div>
+                <h2>{{ __('header.slogan') }}</h2>
+                <h1>{{ __('header.name') }}</h1>
+                <nav>
+                    <a href="#">Menukaart</a>
+                    <a href="#">Nieuws</a>
+                    <a href="#">Contact</a>
+                </nav>
+            </div>
+            <img src="{{ asset('images/dragon-small-flipped.png') }}" alt="" />
+        </aside>
+        <div class="content">
+            {{ $slot }}
+        </div>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
