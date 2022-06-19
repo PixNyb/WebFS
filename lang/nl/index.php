fa5a889243393a8997739cc9b3699e0447443265
Pixel-Null <rcacouwenberg@gmail.com>
2022-06-19 16:50:52 +0000

    Fuked

---
+++ lang/nl/index.php
@@ -0,0 +1,6 @@
+<?php
+
+
+return [
+    'message' => '<p><b>Al jaren is De Gouden Draak een begrip als het gaat om de beste afhaalgerechten in \'s-Hertogenbosch.<br>Graag trakteren we u op authentieke gerechten uit de Cantonese keuken.</b></p><br /><h2><u>Speciale Studentenaanbieding</u></h2><h1>Chinese Rijsttafel (2 personen)</h1><p><b>Maak een keuze uit 3 van de onderstaande keuzegerechten:</b></p><div class="flex row"><span>Koe Loe Yuk</span><span>Tjap Tjoy</span><span>Foe Yong Hai</span><span>Garnalen met Gebakken Knoflook</span><span>Babi Pangang</span><span>Kipfilet in Zwarte Bonen saus</span></div><p><b>Met witte rijst. (Nasi of bami voor meerprijs mogelijk.)</b></p><h1>Prijs: €21,00</h1>'
+];
